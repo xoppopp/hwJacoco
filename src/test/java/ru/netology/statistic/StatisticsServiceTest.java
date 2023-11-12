c2991629ae74f77bec8d0d6304d353e68c379f16
Nikolay Khlyupikov <xoppopp13@gmail.com>
2023-11-12 12:03:19 +0300

    test added

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -17,4 +17,16 @@ public class StatisticsServiceTest {
 
         Assertions.assertEquals(expected, actual);
     }
+
+    @Test
+    void findMaxIfInTheMiddle () {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 5, 8, 44, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 44;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
 }
